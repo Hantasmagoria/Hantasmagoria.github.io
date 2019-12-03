7e81807463f7fcdd0017eb74b161d74113ff0082
Hantasmagoria <30933301+Hantasmagoria@users.noreply.github.com>
2019-12-03 16:45:01 +0800

    bootstrapped the whole thing. still haven't properly introduced react though.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-//Currently empty. will edit later. maybe add react?
+class About extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+class Projects extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+class Contact extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+class App extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
