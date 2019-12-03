28c23a2bb46defb39e2a289ee6709e41d54714d8
Hantasmagoria <30933301+Hantasmagoria@users.noreply.github.com>
2019-12-03 19:42:15 +0800

    redid the css; bootstrapped everything.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 class About extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {" "}
+        <i class="fas fa-h1    "></i>{" "}
+      </div>
+    );
   }
 }
 class Projects extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {" "}
+        <i class="fas fa-h1    "></i>{" "}
+      </div>
+    );
   }
 }
 class Contact extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {" "}
+        <i class="fas fa-h1    "></i>{" "}
+      </div>
+    );
   }
 }
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {" "}
+        <i class="fas fa-h1    "></i>{" "}
+      </div>
+    );
   }
 }
 ReactDOM.render(<App />, document.getElementById("root"));
