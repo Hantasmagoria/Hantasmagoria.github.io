3ae6cd9e6730bd92d068c3fd937c431b8dddb763
Hantasmagoria <30933301+Hantasmagoria@users.noreply.github.com>
2019-12-04 20:16:19 +0800

    react-ified the projects section.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ class About extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      active: false,
       section: [
         {
           title: "About",
@@ -31,26 +32,81 @@ class About extends React.Component {
 
   render() {
     return (
-      <div className="tab-pane active" id="About">
-        {/* {this.state.section.map(section => {
+      <div
+        className={this.state.active ? "tab-pane active" : "tab-pane"}
+        id="About"
+      >
+        {this.state.section.map(section => {
           return (
             <div>
               {" "}
               <h2>{section.title}</h2> <p>{section.content}</p>{" "}
             </div>
           );
-        })} */}
-        <h2>{this.state.section[0].title}</h2>
+        })}
+        {/* <h2>{this.state.section[0].title}</h2>
         <p>{this.state.section[0].content}</p>
         <h2>{this.state.section[1].title}</h2>
-        <p>{this.state.section[1].content}</p>
+        <p>{this.state.section[1].content}</p> */}
       </div>
     );
   }
 }
 class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      projects: [
+        {
+          title: "Overdoze",
+          url:
+            "https://github.com/Hantasmagoria/Hantasmagoria.github.io/blob/master/documentations/overdoze.md",
+          description: `Project 1 for Software Engineering Immersive @ General Assembly.
+        Platform game developed using HTML5 and Javascript.
+        Assets used are from opengameart.org`
+        },
+        {
+          title: "ODDB",
+          url: "https://overdozews.herokuapp.com/",
+          description: `Project 2 for Software Engineering Immersive @ General Assembly.
+        Database-enabled website for leaderboard and score tracking.
+        Inspired by design at osu.ppy.sh`
+        },
+        {
+          title: "Shana the Discord Bot",
+          url:
+            "https://github.com/Hantasmagoria/UnrehearsedAntihistoricalValedictorian",
+          description: `Discord bot catered for the needs of the Invictus discord server. 
+        Uses the discord.js library, hosted on heroku.`
+        },
+        {
+          title: "MCOnline JN script",
+          url: "https://github.com/Hantasmagoria/mcojn",
+          description: `A Javascript Bookmarklet for restructuring the data in the DOM within the facility booking system in MCOnline.sg`
+        }
+      ]
+    };
+  }
+
   render() {
-    return <div className="tab-pane" id="Projects"></div>;
+    return (
+      <div
+        className={this.state.active ? "tab-pane active" : "tab-pane"}
+        id="Projects"
+      >
+        {this.state.projects.map(project => {
+          return (
+            <div>
+              {" "}
+              <a href={project.url} title={project.description}>
+                {project.title}
+              </a>{" "}
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 class Contact extends React.Component {
@@ -78,6 +134,7 @@ class App extends React.Component {
     return (
       <div className="tabbable">
         <About />
+        <Projects />
       </div>
     );
   }
